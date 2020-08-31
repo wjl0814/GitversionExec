4dbcc362a8044e19862bd50ce73b0ffdbceaf05e
wjl0814 <wjl51666@gmail.com>
2020-08-31 23:19:49 +0800

    code format

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 
 namespace WebApplication1.Controllers
 {
@@ -14,13 +13,6 @@ namespace WebApplication1.Controllers
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
-
-        private readonly ILogger<WeatherForecastController> _logger;
-
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
-        {
-            _logger = logger;
-        }
 
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
